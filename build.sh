1d9b629a807c53fbe55077fa2aac8a828de4d3ac
Edmondas Girkantas <edmondas@girkantas.lt>
2024-12-28 19:13:48 +0200

    Be more verbose about progress

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 cd ../poweradmin.github.io/
-cp -R ../poweradmin-docs/screenshots/* screenshots
 
-mkdocs gh-deploy --config-file ../poweradmin-docs/mkdocs.yml --remote-branch master
+mkdocs gh-deploy --verbose --config-file ../poweradmin-docs/mkdocs.yml --remote-branch master
