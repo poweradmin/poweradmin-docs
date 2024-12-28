1e0929c4ddbadddf93edcd62d33607c724b155b8
Edmondas Girkantas <edmondas@girkantas.lt>
2024-12-28 18:35:41 +0200

    Fix build steps

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd ../poweradmin.github.io/
-cp -R ../poweradmin-docs/screenshots screenshots
+cp -R ../poweradmin-docs/screenshots/* screenshots
 
 mkdocs gh-deploy --config-file ../poweradmin-docs/mkdocs.yml --remote-branch master
